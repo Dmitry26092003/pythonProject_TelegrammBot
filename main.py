045c81ff9f39ea6a9f9d1406850e26e19276b888
Dmitry26092003 <Bal.Dima2003@yandex.ru>
2021-09-01 12:08:28 +0300

    add .gitignore

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ def main():
                 I_do_not_understand(user_id)
         elif message.text[0] == '!':
             U.Not_Admin(user_id)
+
         elif (message.text == '/reg') or (  # Регистрация в боте
                 len({"зарегистрироваться", "регистрация"} & set(mes)) != 0):
             R = Reg(bot, db)
